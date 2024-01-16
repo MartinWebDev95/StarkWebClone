2c7f8b7e4ae98fdce4ef7789004e2671ab6df4a0
Juan Antonio <jamll.muro@gmail.com>
2024-01-16 13:39:46 +0100

    Change the companies logos path image

--- src/lib/getCompaniesLogos.js
+++ src/lib/getCompaniesLogos.js
@@ -2,103 +2,103 @@ const getCompaniesLogos = () => {
   const companiesLogo = [
     {
       companyName: 'Charles Schwab logo',
-      logo: '/public/assets/companiesLogo/charles_schwab.svg',
+      logo: '/assets/companiesLogo/charles_schwab.svg',
     },
     {
       companyName: 'US Bank logo',
-      logo: '/public/assets/companiesLogo/us_bank.svg',
+      logo: '/assets/companiesLogo/us_bank.svg',
     },
     {
       companyName: 'American Express logo',
-      logo: '/public/assets/companiesLogo/american_express.svg',
+      logo: '/assets/companiesLogo/american_express.svg',
     },
     {
       companyName: 'Critical Mass logo',
-      logo: '/public/assets/companiesLogo/critical_mass.svg',
+      logo: '/assets/companiesLogo/critical_mass.svg',
     },
     {
       companyName: 'CVS Health logo',
-      logo: '/public/assets/companiesLogo/cvs_health.svg',
+      logo: '/assets/companiesLogo/cvs_health.svg',
     },
     {
       companyName: 'Deloitte logo',
-      logo: '/public/assets/companiesLogo/deloitte.svg',
+      logo: '/assets/companiesLogo/deloitte.svg',
     },
     {
       companyName: 'Dropbox logo',
-      logo: '/public/assets/companiesLogo/dropbox.svg',
+      logo: '/assets/companiesLogo/dropbox.svg',
     },
     {
       companyName: 'FedEx logo',
-      logo: '/public/assets/companiesLogo/fedex.svg',
+      logo: '/assets/companiesLogo/fedex.svg',
     },
     {
       companyName: 'Ford logo',
-      logo: '/public/assets/companiesLogo/ford.svg',
+      logo: '/assets/companiesLogo/ford.svg',
     },
     {
       companyName: 'GE logo',
-      logo: '/public/assets/companiesLogo/ge.svg',
+      logo: '/assets/companiesLogo/ge.svg',
     },
     {
       companyName: 'Google logo',
-      logo: '/public/assets/companiesLogo/google.svg',
+      logo: '/assets/companiesLogo/google.svg',
     },
     {
       companyName: 'IBM logo',
-      logo: '/public/assets/companiesLogo/ibm.svg',
+      logo: '/assets/companiesLogo/ibm.svg',
     },
     {
       companyName: 'Kroger logo',
-      logo: '/public/assets/companiesLogo/kroger.svg',
+      logo: '/assets/companiesLogo/kroger.svg',
     },
     {
       companyName: 'Microsoft logo',
-      logo: '/public/assets/companiesLogo/microsoft.svg',
+      logo: '/assets/companiesLogo/microsoft.svg',
     },
     {
       companyName: 'Mission Lane logo',
-      logo: '/public/assets/companiesLogo/mission_lane.svg',
+      logo: '/assets/companiesLogo/mission_lane.svg',
     },
     {
       companyName: 'NASA logo',
-      logo: '/public/assets/companiesLogo/nasa.svg',
+      logo: '/assets/companiesLogo/nasa.svg',
     },
     {
       companyName: 'Oscar logo',
-      logo: '/public/assets/companiesLogo/oscar.svg',
+      logo: '/assets/companiesLogo/oscar.svg',
     },
     {
       companyName: 'Pfizer logo',
-      logo: '/public/assets/companiesLogo/pfizer.svg',
+      logo: '/assets/companiesLogo/pfizer.svg',
     },
     {
       companyName: 'Salesforce logo',
-      logo: '/public/assets/companiesLogo/salesforce.svg',
+      logo: '/assets/companiesLogo/salesforce.svg',
     },
     {
       companyName: 'SAP logo',
-      logo: '/public/assets/companiesLogo/sap.svg',
+      logo: '/assets/companiesLogo/sap.svg',
     },
     {
       companyName: 'Shopify logo',
-      logo: '/public/assets/companiesLogo/shopify.svg',
+      logo: '/assets/companiesLogo/shopify.svg',
     },
     {
       companyName: 'Union Bank logo',
-      logo: '/public/assets/companiesLogo/union_bank.svg',
+      logo: '/assets/companiesLogo/union_bank.svg',
     },
     {
       companyName: 'United logo',
-      logo: '/public/assets/companiesLogo/united.svg',
+      logo: '/assets/companiesLogo/united.svg',
     },
     {
       companyName: 'University Of Michigan logo',
-      logo: '/public/assets/companiesLogo/university_michigan.svg',
+      logo: '/assets/companiesLogo/university_michigan.svg',
     },
     {
       companyName: 'Volkswagen logo',
-      logo: '/public/assets/companiesLogo/volkswagen.svg',
+      logo: '/assets/companiesLogo/volkswagen.svg',
     },
   ]
 
