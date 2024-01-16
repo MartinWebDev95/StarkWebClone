6389525183b3e6683b0c0136e815aa99f392acba
Juan Antonio <jamll.muro@gmail.com>
2024-01-16 14:24:15 +0100

    Add a new file that return all footer navigation links

---
+++ src/lib/getFooterNavLinks.js
@@ -0,0 +1,96 @@
+const getFooterNavLinks = [
+  {
+    menu: 'Product',
+    submenu: [
+      {
+        submenuItem: 'Stark for figma',
+      },
+      {
+        submenuItem: 'Stark for FigJam',
+      },
+      {
+        submenuItem: 'Stark for Sketch',
+      },
+      {
+        submenuItem: 'Stark for AdobeXD',
+      },
+      {
+        submenuItem: 'Stark for Chrome',
+      },
+      {
+        submenuItem: 'Stark for Firefox',
+      },
+      {
+        submenuItem: 'Stark for Edge',
+      },
+      {
+        submenuItem: 'Stark for Safari',
+      },
+      {
+        submenuItem: 'Stark for Arc',
+      },
+      {
+        submenuItem: 'Stark for Brave',
+      },
+    ]
+  },
+  {
+    menu: 'Resources',
+    submenu: [
+      {
+        submenuItem: 'Blog',
+      },
+      {
+        submenuItem: 'Library',
+      },
+      {
+        submenuItem: 'Release Note',
+      },
+    ]
+  },
+  {
+    menu: 'Help',
+    submenu: [
+      {
+        submenuItem: 'My Account',
+      },
+      {
+        submenuItem: 'Support Docs',
+      },
+      {
+        submenuItem: 'Contact Us',
+      },
+      {
+        submenuItem: 'Featue Requests',
+      },
+    ]
+  },
+  {
+    menu: 'Company',
+    submenu: [
+      {
+        submenuItem: 'Press Releases',
+      },
+      {
+        submenuItem: 'Privacy Policy',
+      },
+      {
+        submenuItem: 'Cookie Policy',
+      },
+      {
+        submenuItem: 'Security Overview',
+      },
+      {
+        submenuItem: 'Status',
+      },
+      {
+        submenuItem: 'Terms of Service',
+      },
+      {
+        submenuItem: 'Code of Conduct',
+      },
+    ]
+  },
+];
+
+export default getFooterNavLinks;
