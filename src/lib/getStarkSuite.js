af39a7dc13632bced28e1880d8a26e39f2de7f9c
Juan Antonio <jamll.muro@gmail.com>
2024-01-16 12:12:27 +0100

    Add a new file that return an object array with all stark suite information

---
+++ src/lib/getStarkSuite.js
@@ -0,0 +1,40 @@
+const getStarkSuite = [
+  {
+    image: '/assets/figma.png',
+    name: 'Stark for Figma',
+  },
+  {
+    image: '/assets/sketch.png',
+    name: 'Stark for Sketch',
+  },
+  {
+    image: '/assets/xd.webp',
+    name: 'Stark for Adobe XD',
+  },
+  {
+    image: '/assets/chrome.png',
+    name: 'Stark for Chrome',
+  },
+  {
+    image: '/assets/firefox.png',
+    name: 'Stark for Firefox',
+  },
+  {
+    image: '/assets/edge.png',
+    name: 'Stark for Edge',
+  },
+  {
+    image: '/assets/safari.png',
+    name: 'Stark for Safari',
+  },
+  {
+    image: '/assets/arc.png',
+    name: 'Stark for Arc',
+  },
+  {
+    image: '/assets/brave.png',
+    name: 'Stark for Brave',
+  },
+];
+
+export default getStarkSuite;
