cf467e15c8890fdc9492ad9c794cfad7accb3e47
Juan Antonio <jamll.muro@gmail.com>
2024-01-16 14:23:25 +0100

    Add a new file that return an object array with all images and names of the social networks

---
+++ src/lib/getSocialList.js
@@ -0,0 +1,32 @@
+const getSocialList = [
+  {
+    image: '/assets/twitter.png',
+    name: 'Twitter',
+  },
+  {
+    image: '/assets/instagram.png',
+    name: 'Instagram',
+  },
+  {
+    image: '/assets/slack.png',
+    name: 'Slack',
+  },
+  {
+    image: '/assets/share-linkedin.webp',
+    name: 'Linkedin',
+  },
+  {
+    image: '/assets/youtube.png',
+    name: 'Youtube',
+  },
+  {
+    image: '/assets/dribbble.png',
+    name: 'Dribbble',
+  },
+  {
+    image: '/assets/meetup.png',
+    name: 'Meetup',
+  },
+];
+
+export default getSocialList;
