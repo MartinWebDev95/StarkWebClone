2c8f07be1b56ff2503e11cfcb5ecf89a094065ef
Juan Antonio <jamll.muro@gmail.com>
2024-01-16 11:49:29 +0100

    Add a function that return all companies logos

---
+++ src/lib/getCompaniesLogos.js
@@ -0,0 +1,108 @@
+const getCompaniesLogos = () => {
+  const companiesLogo = [
+    {
+      companyName: 'Charles Schwab logo',
+      logo: '/public/assets/companiesLogo/charles_schwab.svg',
+    },
+    {
+      companyName: 'US Bank logo',
+      logo: '/public/assets/companiesLogo/us_bank.svg',
+    },
+    {
+      companyName: 'American Express logo',
+      logo: '/public/assets/companiesLogo/american_express.svg',
+    },
+    {
+      companyName: 'Critical Mass logo',
+      logo: '/public/assets/companiesLogo/critical_mass.svg',
+    },
+    {
+      companyName: 'CVS Health logo',
+      logo: '/public/assets/companiesLogo/cvs_health.svg',
+    },
+    {
+      companyName: 'Deloitte logo',
+      logo: '/public/assets/companiesLogo/deloitte.svg',
+    },
+    {
+      companyName: 'Dropbox logo',
+      logo: '/public/assets/companiesLogo/dropbox.svg',
+    },
+    {
+      companyName: 'FedEx logo',
+      logo: '/public/assets/companiesLogo/fedex.svg',
+    },
+    {
+      companyName: 'Ford logo',
+      logo: '/public/assets/companiesLogo/ford.svg',
+    },
+    {
+      companyName: 'GE logo',
+      logo: '/public/assets/companiesLogo/ge.svg',
+    },
+    {
+      companyName: 'Google logo',
+      logo: '/public/assets/companiesLogo/google.svg',
+    },
+    {
+      companyName: 'IBM logo',
+      logo: '/public/assets/companiesLogo/ibm.svg',
+    },
+    {
+      companyName: 'Kroger logo',
+      logo: '/public/assets/companiesLogo/kroger.svg',
+    },
+    {
+      companyName: 'Microsoft logo',
+      logo: '/public/assets/companiesLogo/microsoft.svg',
+    },
+    {
+      companyName: 'Mission Lane logo',
+      logo: '/public/assets/companiesLogo/mission_lane.svg',
+    },
+    {
+      companyName: 'NASA logo',
+      logo: '/public/assets/companiesLogo/nasa.svg',
+    },
+    {
+      companyName: 'Oscar logo',
+      logo: '/public/assets/companiesLogo/oscar.svg',
+    },
+    {
+      companyName: 'Pfizer logo',
+      logo: '/public/assets/companiesLogo/pfizer.svg',
+    },
+    {
+      companyName: 'Salesforce logo',
+      logo: '/public/assets/companiesLogo/salesforce.svg',
+    },
+    {
+      companyName: 'SAP logo',
+      logo: '/public/assets/companiesLogo/sap.svg',
+    },
+    {
+      companyName: 'Shopify logo',
+      logo: '/public/assets/companiesLogo/shopify.svg',
+    },
+    {
+      companyName: 'Union Bank logo',
+      logo: '/public/assets/companiesLogo/union_bank.svg',
+    },
+    {
+      companyName: 'United logo',
+      logo: '/public/assets/companiesLogo/united.svg',
+    },
+    {
+      companyName: 'University Of Michigan logo',
+      logo: '/public/assets/companiesLogo/university_michigan.svg',
+    },
+    {
+      companyName: 'Volkswagen logo',
+      logo: '/public/assets/companiesLogo/volkswagen.svg',
+    },
+  ]
+
+  return companiesLogo.concat(companiesLogo);
+}
+
+export default getCompaniesLogos;
