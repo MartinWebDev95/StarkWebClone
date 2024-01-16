b453f298e726cb0e0f9c04279f05a43b5ef93fa7
Juan Antonio <jamll.muro@gmail.com>
2024-01-16 13:23:00 +0100

    Add a new file that return all started steps in stark

---
+++ src/lib/getStartedSteps.js
@@ -0,0 +1,22 @@
+const getStartedSteps = [
+  {
+    title: 'Start with a free account',
+    subtitle: 'Step 1',
+    description: 'Wherever you are in your accessibility journey, we believe you should have access to the best tools.',
+    btnText: 'Create free account',
+  },
+  {
+    title: 'Download and install Stark',
+    subtitle: 'Step 2',
+    description: 'Download and install our plugins for Figma, Sketch, as well as our browser extensions.',
+    btnText: 'Download Stark',
+  },
+  {
+    title: 'Go Pro when you’re ready',
+    subtitle: 'Step 3',
+    description: 'From Alt-Text and Typography to Touch Targets and Suggestions, there’s much more you can do with a Pro account.',
+    btnText: 'Compare plans',
+  },
+];
+
+export default getStartedSteps;
