319e690264871a375aff082999031d19477c08ea
Juan Antonio <jamll.muro@gmail.com>
2024-01-17 13:31:44 +0100

    Add a new file that return all header navigation links

---
+++ src/lib/getHeaderNavLinks.js
@@ -0,0 +1,70 @@
+const getHeaderNavLinks = [
+  {
+    id: 1,
+    menu: 'Log in',
+  },
+  {
+    id: 2,
+    menu: 'Product',
+    submenu: [
+      {
+        id: 1,
+        submenuItem: 'Stark for figma',
+      },
+      {
+        id: 2,
+        submenuItem: 'Stark for FigJam',
+      },
+      {
+        id: 3,
+        submenuItem: 'Stark for Sketch',
+      },
+      {
+        id: 4,
+        submenuItem: 'Stark for AdobeXD',
+      },
+      {
+        id: 5,
+        submenuItem: 'Stark for Chrome',
+      },
+      {
+        id: 6,
+        submenuItem: 'Stark for Firefox',
+      },
+      {
+        id: 7,
+        submenuItem: 'Stark for Edge',
+      },
+      {
+        id: 8,
+        submenuItem: 'Stark for Safari',
+      },
+      {
+        id: 9,
+        submenuItem: 'Stark for Arc',
+      },
+      {
+        id: 10,
+        submenuItem: 'Stark for Brave',
+      },
+    ]
+  },
+  {
+    id: 3,
+    menu: 'Pricing',
+  },
+  {
+    id: 4,
+    menu: 'Blog',
+  },
+  {
+    id: 5,
+    menu: 'Library',
+  },
+  {
+    id: 6,
+    menu: 'Support',
+  },
+];
+
+export default getHeaderNavLinks;
