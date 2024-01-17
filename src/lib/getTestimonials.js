49d918eef58a6b73f427bb87417d64e155cd2566
Juan Antonio <jamll.muro@gmail.com>
2024-01-17 08:41:28 +0100

    Add a new file that return an object array with the different testimonials

---
+++ src/lib/getTestimonials.js
@@ -0,0 +1,46 @@
+const getTestimonials = [
+  {
+    id: 1,
+    profile: '/assets/carlos-torres.png',
+    testimonial: '"I have learned so much and how to think of others while designing."',
+    author: 'Carlos Torres',
+    job: 'UX Engineer at Alto',
+  },
+  {
+    id: 2,
+    profile: '/assets/emily-ching.png',
+    testimonial: '"Stark is my favorite plugin for Figma! I use it daily."',
+    author: 'Emily Ching',
+    job: 'Head of design at Making CO',
+  },
+  {
+    id: 3,
+    profile: '/assets/johnny-hill.png',
+    testimonial: '"Everyone should be using Stark as part of their workflow."',
+    author: 'Johnny Hill',
+    job: 'UX Director at Knock, INC',
+  },
+  {
+    id: 4,
+    profile: '/assets/jon-fox.png',
+    testimonial: '"...a welcome tool set to make interesting, impactful designs that are also accessible."',
+    author: 'Jon Fox',
+    job: 'Lead UX Designer at LA Voting Solutions',
+  },
+  {
+    id: 5,
+    profile: '/assets/jordan-lucero.png',
+    testimonial: '"...from the ease of use, fantastic resources, and the amount of support it gets from the team."',
+    author: 'Jordan Lucero',
+    job: 'CEO at JServices',
+  },
+  {
+    id: 6,
+    profile: '/assets/sean-keating.png',
+    testimonial: '"It\'s simple, easy to use, and effective. Does just what I need it to do."',
+    author: 'Sean Keating',
+    job: 'Digital Product Designer at S&P Global',
+  },
+];
+
+export default getTestimonials;
